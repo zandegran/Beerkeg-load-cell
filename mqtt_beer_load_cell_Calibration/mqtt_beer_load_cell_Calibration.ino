f7a987f0c52ed4608eaa4c01ba49c21ef4f1ca66
Ansel Zandegran <zandegran@gmail.com>
2020-10-16 17:55:16 +0200

    Improve calibration

--- mqtt_beer_load_cell_Calibration/mqtt_beer_load_cell_Calibration.ino
+++ mqtt_beer_load_cell_Calibration/mqtt_beer_load_cell_Calibration.ino
@@ -1,55 +1,117 @@
-#include <HX711.h>
+#include "HX711.h"
 
 HX711 scale;  // Initializes library functions.
 
+const float KNOWN_WEIGHT = 5.76;  // In kilograms
+
 const int LOADCELL_DOUT_PIN = 2;          //Remember these are ESP GPIO pins, these might not be the pins labeled on the board.
-const int LOADCELL_SCK_PIN = 0;
+const int LOADCELL_SCK_PIN = 3;
 
-int calibration_factor = -22500
+const int INITIAL_INCREMENTS = 50;
+const float FLUCTUATION_TOLERANCE = 0.3;
 
+long calibration_factor = -500;
 
+int foundUpperBound = 0;
 
-
-
-; // Defines calibration factor we'll use for calibrating.
 
 void setup()
 {
-  Serial.begin(74880);   // Starts serial communication in 9600 baud rate.
+  Serial.begin(74880);   
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 
   Serial.println("Initializing scale calibration.");  // Prints user commands.
   Serial.println("Please remove all weight from scale.");
-  Serial.println("Place known weights on scale one by one.");
-  Serial.println("Press + or a to increase calibration factor");
-  Serial.println("Press - or z to decrease calibration factor");
 
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);   // Initializes the scaling process.
+  Serial.println("began");
   scale.set_scale();
+  Serial.println("scale set");
+  digitalWrite(LED_BUILTIN, HIGH);
   scale.tare();          // Resets the scale to 0.
+  digitalWrite(LED_BUILTIN, HIGH); 
+  Serial.println("Please place the weight");
+  delay(5000);
 }
 
+void printBounds(float lowerBound, float upperBound) {
+   float midpoint = lowerBound + ((upperBound - lowerBound) / 2);
+   Serial.println("#############################");
+   Serial.println("##   LB:"+ String(lowerBound) + " UB: " + String(upperBound) + "   ##");
+   Serial.println("##   CF:"+ String(midpoint) +  "            ##");
+   Serial.println("#############################");
+}
+
+void binarySearch(float lowerBound, float upperBound) {
+  float wtForUpperBound = getWeight(upperBound);
+  float wtForLowerBound = getWeight(lowerBound);
+  float wt = 0;
+  do {
+    float midpoint = lowerBound + ((upperBound - lowerBound) / 2);
+    wt = getWeight(midpoint);
+    if (wt == KNOWN_WEIGHT) {
+      break;
+    }
+    if (wt > KNOWN_WEIGHT) {
+      lowerBound = midpoint;
+      wtForLowerBound  = wt;
+    }
+    else if (wt < KNOWN_WEIGHT) {
+      upperBound = midpoint;
+      wtForUpperBound = wt;
+    }
+    Serial.println("LB:  " + String(lowerBound) +" UB:  " + String(wtForUpperBound));
+    Serial.println("LBW: " + String(wtForLowerBound) +" UBW: " + String(wtForUpperBound));
+    if (wt < wtForLowerBound || wt > wtForUpperBound) {
+      // Too much fluctuation retrying
+      return; 
+     }
+  } while (lowerBound < upperBound); // Loop will fail when the fluctuation is high
+  printBounds(lowerBound, upperBound);
+}
+
+float getWeight(float calibrationFactor) {
+  digitalWrite(LED_BUILTIN, LOW); 
+  scale.power_up();
+  scale.set_scale(calibration_factor); 
+  scale.wait_ready();
+  float wt = scale.get_units(10);
+  Serial.println("Reading: " + String(wt) +" cf: " + String(calibrationFactor));
+  scale.power_down();
+  digitalWrite(LED_BUILTIN, foundUpperBound == 0 ? LOW : HIGH); // Dont turn off led on no weight
+  delay(3000);
+  return wt;
+}
+
+bool isWeightOnScale(float fluctuationTolerance) {
+  float wt = getWeight(calibration_factor);
+  Serial.println("wt on scale: " + String(wt));
+  wt *= wt < 0 ? -1 : 1; // ABS
+  wt > fluctuationTolerance;
+}
+
+int getUpperBound() {
+  // Seek upper bound
+  float wt = getWeight(calibration_factor);
+  while (wt < KNOWN_WEIGHT && wt > 0) {
+    calibration_factor += INITIAL_INCREMENTS;
+    if (calibration_factor != 0) {
+      wt = getWeight(calibration_factor);
+    }
+    Serial.println(wt);
+    Serial.println("Seeking bounds: " + String(calibration_factor)+ " current wt: " + String(wt));
+  }
+  Serial.println("UPPER BOUND FOUND" + String(calibration_factor));
+  return calibration_factor;
+}
 
 void loop()
 {
-
-  scale.set_scale(calibration_factor);  // Adjusts the calibration factor.
-  scale.wait_ready();
-  Serial.print("Reading: ");            // Prints weight readings in .2 decimal kg units.
-  Serial.print(scale.get_units(), 2);
-  Serial.println(" kg");
-  Serial.print("Calibration factor: "); // Prints calibration factor.
-  Serial.println(calibration_factor);
-
-  scale.power_down();    // Puts the scale to sleep mode for 3 seconds.
-  delay(3000);
-  scale.power_up();
-
-  if(Serial.available())
-  {
-    char temp = Serial.read();
-    if(temp == '+' || temp == 'a')
-      calibration_factor += 500;
-    else if(temp == '-' || temp == 'z')
-      calibration_factor -= 500;  
-  }
+    if (foundUpperBound == 0) { // Execute once after weight is placed
+      foundUpperBound = getUpperBound();
+    }
+    binarySearch(foundUpperBound - INITIAL_INCREMENTS, foundUpperBound + INITIAL_INCREMENTS);
+  
+  delay(1000);  
 }
